288dd73dc30bafe462fdf38237df751efc4070da
sapsari <sapsari@gmail.com>
2024-09-30 14:44:57 +0300

    sentry added

--- sentry-error/out/extension.js
+++ sentry-error/out/extension.js
@@ -28,6 +28,7 @@ exports.deactivate = deactivate;
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = __importStar(require("vscode"));
+const browser_1 = require("@sentry/browser");
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 function activate(context) {
@@ -42,6 +43,25 @@ function activate(context) {
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World from sentry-error!');
     });
+    // filter integrations that use the global variable
+    const integrations = (0, browser_1.getDefaultIntegrations)({}).filter((defaultIntegration) => {
+        return ![
+            "BrowserApiErrors",
+            "Breadcrumbs",
+            "GlobalHandlers",
+        ].includes(defaultIntegration.name);
+    });
+    const client = new browser_1.BrowserClient({
+        dsn: "___PUBLIC_DSN___",
+        transport: browser_1.makeFetchTransport,
+        stackParser: browser_1.defaultStackParser,
+        integrations: integrations,
+    });
+    const scope = new browser_1.Scope();
+    scope.setClient(client);
+    client.init(); // initializing has to be done after setting the client on the scope
+    // You can capture exceptions manually for this client like this:
+    scope.captureException(new Error("example"));
     context.subscriptions.push(disposable);
 }
 // This method is called when your extension is deactivated
--- sentry-error/src/extension.ts
+++ sentry-error/src/extension.ts
@@ -2,6 +2,14 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import {
+	BrowserClient,
+	defaultStackParser,
+	getDefaultIntegrations,
+	makeFetchTransport,
+	Scope,
+  } from "@sentry/browser";
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -18,6 +26,36 @@ export function activate(context: vscode.ExtensionContext) {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from sentry-error!');
 	});
+
+
+
+
+	// filter integrations that use the global variable
+const integrations = getDefaultIntegrations({}).filter((defaultIntegration) => {
+	return ![
+	  "BrowserApiErrors",
+	  "Breadcrumbs",
+	  "GlobalHandlers",
+	].includes(defaultIntegration.name);
+  });
+  
+  const client = new BrowserClient({
+	dsn: "___PUBLIC_DSN___",
+	transport: makeFetchTransport,
+	stackParser: defaultStackParser,
+	integrations: integrations,
+  });
+  
+  const scope = new Scope();
+  scope.setClient(client);
+  
+  client.init(); // initializing has to be done after setting the client on the scope
+  
+  // You can capture exceptions manually for this client like this:
+  scope.captureException(new Error("example"));
+
+
+
 
 	context.subscriptions.push(disposable);
 }
